59f19210716da56b5be6a5276756aed058e25c62
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-02-16 13:24:20 +0530

    feat: Types of all fetched data

---
+++ src/types/index.ts
@@ -0,0 +1,88 @@
+export type Profile = {
+    realName: string;
+    websites: string[];
+    countryName: string | null;
+    skillTags: string[];
+    company: string | null;
+    school: string | null;
+    starRating: number;
+    aboutMe: string;
+    userAvatar: string;
+    reputation: number;
+    ranking: number;
+}
+
+export type MatchedUser = {
+    username: string;
+    socialAccounts: unknown;
+    githubUrl: string | null;
+    linkedinUrl: string | null;
+    profile: Profile;
+    activeBadge: {
+        id: string;
+        displayName: string;
+        icon: string;
+    } | null
+}
+
+export type Contest = {
+    title: string;
+    startTime: number;
+}
+
+export type ContestInfo = {
+    attended: boolean;
+    trendDirection: string;
+    problemsSolved: number;
+    totalProblems: number;
+    finishTimeInSeconds: number;
+    rating: number;
+    ranking: number;
+    contest: Contest;
+}
+
+export type ContestRanking = {
+    attendedContestsCount: number;
+    rating: number;
+    globalRanking: number;
+    totalParticipants: number;
+    topPercentage: number;
+    badge: null | {
+        name: string;
+    };
+}
+
+export type UserContestInfo = {
+    userContestRanking: ContestRanking;
+    userContestRankingHistory: ContestInfo[];
+}
+
+export type AllQuestionsCount = {
+    difficulty: string;
+    count: number;
+}
+
+export type AcSubmissionNum = {
+    difficulty: string;
+    count: number;
+    submissions: number;
+}
+
+export type TotalSubmissionNum = {
+    difficulty: string;
+    count: number;
+    submissions: number;
+}
+
+export type SubmitStats = {
+    acSubmissionNum: AcSubmissionNum[];
+    totalSubmissionNum: TotalSubmissionNum[];
+}
+
+export type RecentSubmission = {
+    title: string;
+    titleSlug: string;
+    timestamp: string;
+    statusDisplay: string;
+    lang: string;
+}
