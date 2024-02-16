a02f230efa05a218eff2925761a507b7fcc085b2
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-02-16 13:28:23 +0530

    fix: Setup proxy details to avoid cors errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+    cors:  true,
+    proxy: {
+      '/leetcode': {
+        target: 'https://leetcode.com/graphql',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
 })
