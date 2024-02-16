2cabe8922b09d4d7aeb2c04417420522d8fbe76b
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-02-16 13:23:05 +0530

    feat: Queries to fetch leetcode data
    - ass leetcode api requires graphql type queries
    we need ptovide in that structure

---
+++ src/utils/queries.ts
@@ -0,0 +1,109 @@
+const profileQuery =`
+query ($username: String!) {
+    matchedUser(username: $username) {
+        username
+        socialAccounts
+        githubUrl
+        linkedinUrl
+        profile {
+            realName
+            websites
+            countryName
+            skillTags
+            company
+            school
+            starRating
+            aboutMe
+            userAvatar
+            reputation
+            ranking
+        }
+        activeBadge {
+            id
+            displayName
+            icon
+        }
+    }
+}
+`;
+
+const contestDetailsQuery = `
+query ($username: String!) {
+    userContestRanking(username: $username) {
+        attendedContestsCount
+        rating
+        globalRanking
+        totalParticipants
+        topPercentage
+        badge {
+            name
+        }
+    }
+    userContestRankingHistory(username: $username) {
+        attended
+        trendDirection
+        problemsSolved
+        totalProblems
+        finishTimeInSeconds
+        rating
+        ranking
+        contest {
+            title
+            startTime
+        }
+    }
+}
+`;
+
+const solvedProblemsStatsQuery = `
+query ($username: String!) {
+    allQuestionsCount {
+        difficulty
+        count
+    }
+    matchedUser(username: $username) {
+        submitStats {
+            acSubmissionNum {
+                difficulty
+                count
+                submissions
+            }
+            totalSubmissionNum {
+                difficulty
+                count
+                submissions
+            }
+        }
+    }
+}
+`;
+
+const recentSubmissionQuery = `
+query ($username: String!, $limit: Number) {
+    recentSubmissionList(username: $username, limit: $limit) {
+        title
+        titleSlug
+        timestamp
+        statusDisplay
+        lang
+    }
+}
+`;
+
+
+const heatMapQuery = `
+query ($username: String!) {
+    matchedUser(username: $username) {
+        submissionCalendar
+    }
+}
+`;
+
+
+export {
+    profileQuery, 
+    contestDetailsQuery, 
+    solvedProblemsStatsQuery, 
+    recentSubmissionQuery,
+    heatMapQuery
+}
