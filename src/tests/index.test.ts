948306c0842dc38b723712be373d8ebcf8853d72
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-02-21 13:38:32 +0530

    chore: testing setup for the project

---
+++ src/tests/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest";
+
+describe('Test', () => { 
+    it("should run the test", () => {
+        expect(true).toBe(true);
+    })
+ })
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup",
+  },
   server: {
     open: true,
     cors:  true,
@@ -14,5 +19,5 @@ export default defineConfig({
         secure: false,
       },
     }
-  }
+  },
 })
