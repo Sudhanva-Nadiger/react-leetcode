6f59e1d037128f7c755996200da91e34b07ff024
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-02-22 16:31:15 +0530

    test: add  tests for loadingOrError component

---
+++ src/tests/components/loadingOrError.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import LoadingOrError from '../../components/LoadingOrError'
+
+describe("Tests for loading or error component", () => {
+    it("Should render loading ui", () => {
+        const { container } = render(<LoadingOrError loading={true} />);
+        const loadingContainer = container.querySelector("#loadig_container");
+        expect(loadingContainer).toBeInTheDocument();
+
+        const loadingText = screen.getByText("Loading...");
+        expect(loadingText).toBeInTheDocument();
+    })
+
+    it("Should render user defined loading component", () => {
+        const { container } = render(<LoadingOrError loading={true} loadingComponent={<div/>} />);
+        const userDefinedLoadingComponent = container.querySelector("#user_defined_loading_component");
+        const loadingContainer = container.querySelector("#loadig_container");
+        expect(loadingContainer).not.toBeInTheDocument();
+        expect(userDefinedLoadingComponent).toBeInTheDocument();
+    })
+
+    it("Should render error ui", () => {
+        const { container } = render(<LoadingOrError error={true} />);
+
+        const errorContainer = container.querySelector("#error_container");
+        expect(errorContainer).toBeInTheDocument();
+
+        const errorTest = screen.getByText("Somethinng went wrong, Could not load data");
+        expect(errorTest).toBeInTheDocument();
+    })
+
+    it("Should not render anything", () => {
+        const { container } = render(<LoadingOrError />);
+        expect(container).toBeEmptyDOMElement();
+    })
+})
