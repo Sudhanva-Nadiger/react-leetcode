89198c4d10e99a5eb0fdf898e2e2f46e4e9312a6
Sudhanva-Nadiger <sudhanvanadiger12@gmail.com>
2024-02-25 10:44:29 +0530

    trying for chunks of build

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,6 +6,7 @@ import './App.css'
 function App() {
   return (
     <>
+      <h1 className='text-red-500'>Hello world</h1>
       <UserProfileDetails userName='sudhanva_nadiger__'/>
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { resolve, extname, relative } from 'path';
 import dts from 'vite-plugin-dts';
 import tailwindcss from 'tailwindcss';
 import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { glob } from 'glob'
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,11 +13,26 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'react-leetcode',
+      formats: ['es'],
       fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
       external: ['react', 'react-dom', 'tailwindcss'],
+      input: Object.fromEntries(
+               glob.sync('src/**/*.{ts,tsx}').map(file => [
+                 // The name of the entry point
+                 // lib/nested/foo.ts becomes nested/foo
+                 relative(
+                   'src',
+                   file.slice(0, file.length - extname(file).length)
+                 ),
+                 // The absolute path to the entry file
+                 // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                 fileURLToPath(new URL(file, import.meta.url))
+               ])
+             ),
       output: {
+        entryFileNames: '[name].js',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
@@ -29,11 +46,11 @@ export default defineConfig({
   },
 
 
-  plugins: [react(), dts(
-    {
-      exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.tsx'],
-    }
-  ), libInjectCss()],
+  plugins: [
+    react(), 
+    dts({ exclude: ['src/tests'] }), 
+    libInjectCss()
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss('./tailwind.config.js')],
